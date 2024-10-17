410129bff007c31eb961c1de9f97ce1729737e76
Alessia S Fitz Gibbon <asfg@cs.washington.edu>
2024-10-17 15:57:08 -0700

    update examples

---
+++ examples/get_notes.py
@@ -0,0 +1,65 @@
+import os
+import pandas as pd
+
+from edapi import EdAPI
+from ed_markdown_converter import EdTagsConverter
+
+
+NOTES_DIR = ""  # TODO: replace with path to notes directory
+COURSE_ID = 0   # TODO: replace with your course id
+
+
+# Create shorthand method for conversion
+def md(html, **options):
+    return EdTagsConverter(**options).convert(html)
+
+
+def parse_date(timestamp):
+    datetime = pd.to_datetime(timestamp)
+    return datetime.tz_convert("America/Los_Angeles")
+
+
+def main():
+    ed = EdAPI()
+    ed.login()
+
+    notes = sorted(
+        ed.list_all_threads(COURSE_ID),
+        key=lambda a: parse_date(a["created_at"])
+    )
+
+    notes = list(filter(
+        lambda a:
+            a["type"] == "announcement" and a["category"] == "Lectures"
+            and a["subcategory"] == "Notes",
+        notes))
+
+    for post in notes:
+        title = post["title"]
+        path = os.path.join(NOTES_DIR, f"{title}")
+        os.makedirs(path, exist_ok=True)
+
+        # raw HTML
+        with open(os.path.join(path, "content.html"), "w") as f:
+            f.write(post["content"])
+
+        # Process Markdown
+        image_path = os.path.join(path, "images")
+        os.makedirs(image_path, exist_ok=True)
+        with open(os.path.join(path, "content.md"), "w") as f:
+            f.write(
+                md(post["content"],
+                   image_path=image_path,
+                   download_images=True)
+            )
+
+        # Process with embedded images
+        # NOTE: Use with caution. This creates quite large files, and Ed
+        #       struggles with pasting them in.
+        # with open(os.path.join(path, "content-embedded.md"), "w") as f:
+        #     f.write(md(post["content"], embed_images=True))
+
+
+if __name__ == "__main__":
+    main()
+    # convert_notes_to_md()
--- examples/list_thread_titles.py
+++ examples/list_thread_titles.py
@@ -1,11 +1,13 @@
 from edapi import EdAPI
 
+COURSE_ID = 0  # TODO: replace with your course id
+
 
 def main():
     ed = EdAPI()
     ed.login()
 
-    threads = ed.list_all_threads(67447)
+    threads = ed.list_all_threads(COURSE_ID)
     for thread in threads:
         print(f"#{thread['number']} ({thread['id']}) {thread['title']}")
 
